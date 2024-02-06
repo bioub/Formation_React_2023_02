92e90afac9ecbe28ed7475eee8ee3ed341c259e1
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2024-02-06 14:37:30 +0100

    useEffect / useRef

--- hello-react/src/Clock.tsx
+++ hello-react/src/Clock.tsx
@@ -1,13 +1,42 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function Clock() {
-  console.log('Clock renders');
+  // console.log('Clock renders');
   
   const [now, setNow] = useState(new Date());
+  // const [name, setName] = useState('Romain');
 
-  setTimeout(() => {
-    setNow(new Date());
-  }, 1000);
+  // useEffect(() => {
+  //   console.log('called after each render');
+  //   return () => {
+  //     console.log('before each effect, except the first one and when the component will destroy')
+  //   }
+  // })
+
+  // useEffect(() => {
+  //   console.log('called after the first render');
+  // return () => {
+  //   console.log('when the component will destroy')
+  // }
+  // }, [])
+
+  // useEffect(() => {
+  //   console.log('called after the first render + after each render when the value(s) present in the array changes');
+  // return () => {
+  //   console.log('before each effect, except the first one (only when value in the array changes) and when the component will destroy')
+  // }
+  // }, [name])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setNow(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+  
 
   return <div className="Clock">Il est {now.toLocaleTimeString()}</div>;
 }
--- hello-react/src/Select.tsx
+++ hello-react/src/Select.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export interface SelectProps {
   items: string[];
@@ -8,6 +8,7 @@ export interface SelectProps {
 
 function Select({ items, value, onValueChange }: Readonly<SelectProps>) {
   const [menuOpen, setMenuOpen] = useState(false);
+  const hostRef = useRef<HTMLDivElement>(null);
 
   function handleItemClick(newItem: string) {
     // lift newItem to the parent
@@ -15,13 +16,27 @@ function Select({ items, value, onValueChange }: Readonly<SelectProps>) {
     setMenuOpen(false);
   }
 
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (!hostRef.current?.contains(event.target as Node)) {
+        setMenuOpen(false)
+      }
+    };
+    window.addEventListener('click', listener);
+    return () => {
+      window.removeEventListener('click', listener);
+    };
+  }, []);
+
   return (
-    <div>
+    <div ref={hostRef}>
       <div onClick={() => setMenuOpen(!menuOpen)}>{value}</div>
       {menuOpen && (
         <div>
           {items.map((item) => (
-            <div key={item} onClick={() => handleItemClick(item)}>{item}</div>
+            <div key={item} onClick={() => handleItemClick(item)}>
+              {item}
+            </div>
           ))}
         </div>
       )}
